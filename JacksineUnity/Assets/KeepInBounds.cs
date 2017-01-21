f8eed53f297f07b47fbed2fda2705a98fd14a1f1
Ryan McCampbell <rmccampbell7@gmail.com>
2017-01-21 15:39:48 -0500

    keep in bounds

--- JacksineUnity/Assets/KeepInBounds.cs
+++ JacksineUnity/Assets/KeepInBounds.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class KeepInBounds : MonoBehaviour {
 
+    public static float FORCE = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,28 @@ public class KeepInBounds : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector2 pos = transform.position;
+        Vector2 botLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 topRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        if (pos.x < botLeft.x)
+        {
+            print("out left");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * FORCE);
+        }
+        if (pos.x >= topRight.x)
+        {
+            print("out right");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * FORCE);
+        }
+        if (pos.y < botLeft.y)
+        {
+            print("out bottom");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * FORCE);
+        }
+        if (pos.y >= topRight.y)
+        {
+            print("out top");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.down * FORCE);
+        }
 	}
 }
