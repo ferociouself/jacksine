d93967e8bc46270ef7143b0a7e419cb3947da33e
ferociouself <jhe3cd@virginia.edu>
2017-01-21 01:52:05 -0500

    Particles Merge
    Merging Particles Branch Manually

---
+++ JacksineUnity/Assets/Scripts/MovementBleepScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBleepScript : MonoBehaviour {
+	public float speedMultiplier;
+	public float lowPitch;
+	public float midPitch;
+	public float hiPitch;
+	public float lowThreshold;
+	public float hiThreshold;
+	public float cooldownThreshold;	// amount of time between audio play
+
+	private Rigidbody2D body;
+	private float pitch;
+	private float cooldownTimer;
+
+	// Use this for initialization
+	void Start () {
+		body = this.GetComponent<Rigidbody2D> ();
+		pitch = this.GetComponent<AudioSource> ().pitch;
+		cooldownTimer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speed = body.velocity.magnitude*speedMultiplier;
+		if (speed > 0.001 && cooldownTimer >= cooldownThreshold) {
+			AudioSource audio = this.GetComponent < AudioSource> ();
+			if (speed > hiThreshold) {
+				audio.pitch = hiPitch;
+			} else if (speed < lowThreshold) {
+				audio.pitch = lowPitch;
+			} else {
+				audio.pitch = midPitch;
+			}
+			audio.Play ();
+			cooldownTimer = 0.0f;
+		}
+
+		cooldownTimer += Time.deltaTime;
+	}
+}
