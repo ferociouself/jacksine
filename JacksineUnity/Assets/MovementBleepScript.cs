594a741617c5b38765782fa5ab4e1fd436d89c23
SebastianJay <jgs3cd@virginia.edu>
2017-01-21 00:48:56 -0500

    modify bleep script

--- JacksineUnity/Assets/MovementBleepScript.cs
+++ JacksineUnity/Assets/MovementBleepScript.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class MovementBleepScript : MonoBehaviour {
 	public float speedMultiplier;
+	public float lowPitch;
+	public float midPitch;
+	public float hiPitch;
+	public float lowThreshold;
+	public float hiThreshold;
+	public float cooldownThreshold;	// amount of time between audio play
+
 	private Rigidbody2D body;
 	private float pitch;
+	private float cooldownTimer;
 
 	// Use this for initialization
 	void Start () {
 		body = this.GetComponent<Rigidbody2D> ();
 		pitch = this.GetComponent<AudioSource> ().pitch;
+		cooldownTimer = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float speed = body.velocity.magnitude*speedMultiplier;
-		if (speed > 0.001) {
+		if (speed > 0.001 && cooldownTimer >= cooldownThreshold) {
 			AudioSource audio = this.GetComponent < AudioSource> ();
-			if (speed > 8) {
-				audio.pitch = 3;
-			} else if (speed < 1.1f) {
-				audio.pitch = 0.2f;
+			if (speed > hiThreshold) {
+				audio.pitch = hiPitch;
+			} else if (speed < lowThreshold) {
+				audio.pitch = lowPitch;
 			} else {
-				audio.pitch = 3 - (3 / speed);
+				audio.pitch = midPitch;
 			}
 			audio.Play ();
+			cooldownTimer = 0.0f;
 		}
+
+		cooldownTimer += Time.deltaTime;
 	}
 }
