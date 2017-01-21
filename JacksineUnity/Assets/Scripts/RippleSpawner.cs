1f71e33922560136c9cebc21c5934b048a9375b1
Divya354 <dsk5df@virginia.edu>
2017-01-21 14:15:14 -0500

    charge

--- JacksineUnity/Assets/Scripts/RippleSpawner.cs
+++ JacksineUnity/Assets/Scripts/RippleSpawner.cs
@@ -6,10 +6,13 @@ using Tobii.EyeTracking;
 public class RippleSpawner : MonoBehaviour {
 
 	public bool useEyeTracking = true;
+	public float time;
     ObjectController objCont;
 
 	// Use this for initialization
 	void Start () {
+		time = 0;
+		Debug.Log ("Time Scale: " + Time.timeScale);
         objCont = gameObject.GetComponent<ObjectController>();
 		if (useEyeTracking) {
 			EyeTracking.Initialize ();
@@ -18,9 +21,10 @@ public class RippleSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Shoot"))
-        {
-            Debug.Log("Shooting!");
+
+
+		if (Input.GetButtonDown ("Shoot")) {
+			Debug.Log ("Shooting!");
 			Vector3 posVec;
 			if (useEyeTracking) {
 				GazePoint point = EyeTracking.GetGazePoint ();
@@ -28,8 +32,24 @@ public class RippleSpawner : MonoBehaviour {
 			} else {
 				posVec = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			}
-            posVec.z = 0;
-            objCont.CreateRipple((Vector2)posVec);
-        }
+			posVec.z = 0;
+			GameObject ripple = objCont.CreateRipple ((Vector2)posVec);
+
+			//scale ripple by time waited
+			ripple.transform.GetChild (0).gameObject.GetComponent<Embiggener>().maxSize*=(time/5.0f);
+			ripple.transform.GetChild (1).gameObject.GetComponent<Embiggener>().maxSize*=(time/5.0f);
+			ripple.transform.GetChild (2).gameObject.GetComponent<Embiggener>().maxSize*=(time/5.0f);
+			ripple.transform.GetChild (3).gameObject.GetComponent<Embiggener>().maxSize*=(time/5.0f);
+
+
+			time=0; 
+
+
+		} else {
+
+			if(time<=5.0f){
+			time+=Time.deltaTime;
+			}
+		}
 	}
 }
