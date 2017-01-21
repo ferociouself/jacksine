d01969f1c2067b59625f4b918ff7cdebc34e1b21
jpat64 <jrp3tu@virginia.edu>
2017-01-21 12:41:36 -0500

    Added island interaction and a dummy ripple that
    dies lel

---
+++ JacksineUnity/Assets/Scripts/IslandRippleDyingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandRippleDyingScript : MonoBehaviour {
+
+    public float timer = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += 0.01f;
+        if (timer > 3.0f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+	}
+}
